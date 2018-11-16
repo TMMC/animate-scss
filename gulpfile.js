ea977f3b5a961fa98a70e4943a8cd7d4b70ebc7c
TMMC <TMMC@users.noreply.github.com>
2018-11-16 15:35:35 +0100

    More initial changes
    WARNING it is work in progress, currently not for usage in any case.

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+"use strict";
+
+const pkg                    = require('./package.json'),
+path                         = require('path'),
+{ exec }                     = require('child_process'),
+del                          = require('del'),
+{ src, dest, watch, series } = require('gulp'),
+header                       = require('gulp-header'),
+rename                       = require('gulp-rename'),
+sass                         = require('gulp-sass'),
+autoprefixer                 = require('gulp-autoprefixer'),
+ansi                         = require('ansi-colors'),
+banner                       = [
+  '/*!',
+  ' * ' + pkg.name + ' - v.' + pkg.version,
+  ' * Licensed under the MIT license - http://opensource.org/licenses/MIT',
+  ' * Copyright (c) ' + new Date().getFullYear() + ' ' + pkg.author.name + ' - ' + pkg.author.url,
+  ' * ',
+  ' * ' + pkg.description + ' ' + pkg.homepage,
+  ' * Copyright (c) ' + new Date().getFullYear() + ' Daniel Eden - https://daneden.me/',
+  ' */',
+  '\n'
+].join('\n');
+
+sass.compiler                = require('node-sass');
+
+// ==============================================
+// === Standalone tasks
+// ==============================================
+
+// == stylesheets: process SASS files (minify if in production mode) and autoprefix compiled CSS
+function stylesheets() {
+  return src(pkg.src + '/animate.scss')
+    .pipe(sass(pkg.sassOptions.dev).on('error', sass.logError))
+    .pipe(header(banner))
+    .pipe(autoprefixer(pkg.autoprefixerOptions))
+    .pipe(dest(pkg.dist))
+    .pipe(sass(pkg.sassOptions.prod).on('error', sass.logError))
+    .pipe(rename({ extname : '.min.css' }))
+    .pipe(autoprefixer(pkg.autoprefixerOptions))
+    .pipe(dest(pkg.dist));
+}
+stylesheets.description = 'Process SASS files and autoprefix compiled CSS.';
+exports.stylesheets = stylesheets;
+
+// == watcher: watch for changes in SASS sources to run corresponding task
+function watcher() {
+  watch(pkg.src + '/**/*.{scss,sass}', { ignoreInitial: false }, stylesheets);
+}
+watcher.description = 'Watch for changes in SASS sources to run corresponding task.';
+exports.watcher = watcher;
+
+// == clean: remove dist directory
+function clean() {
+  return del(pkg.dist).then(paths => {
+    if (paths.length) {
+      console.log(ansi.yellow.bold('Deleted files and folders:\n'), ansi.yellow(paths.join('\n')));
+    } else {
+      console.log(ansi.yellow.bold('Location already cleaned.'));
+    }
+  });
+}
+clean.description = 'Clean and remove `dist` directory.';
+exports.clean = clean;
+
+// == availTasks: log available tasks to console
+function availTasks(done) {
+  var command = 'gulp --tasks-simple';
+  if (process.argv.indexOf('--verbose') > -1) {
+    command = 'gulp --tasks';
+  }
+  exec(command, function (err, stdout, stderr) {
+    done(console.log(ansi.cyan.bold('Available tasks are:\n') + ansi.cyan(stdout)));
+  });
+}
+availTasks.description = 'Log available tasks to console as plain text list.';
+availTasks.flags = {
+  '--verbose': 'Display tasks dependency tree instead of plain text list.'
+};
+// exports.availTasks = availTasks;
+
+// == default: export `availTasks` as default task
+exports.default = availTasks;
+
+// ==============================================
+// === Combo tasks
+// ==============================================
+
+// == build: build project from scratch (clean and compile)
+var build = series(clean, stylesheets);
+build.description = 'Build project from scratch (clean and compile).';
+exports.build = build;
+
+// == buildWatch: build project from scratch (clean and compile) and watch for changes
+var buildWatch = series(clean, watcher);
+buildWatch.description = 'Build project from scratch (clean and compile) and watch for changes.';
+exports.buildWatch = buildWatch;
